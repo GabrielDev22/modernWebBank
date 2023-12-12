59d2982c61d7c8ed5a7fb840ecb3bcce5e87cde9
Gabriel <gabrieltorresmora2@gmail.com>
2023-12-12 08:25:28 -0300

    subiendo todos los avances realizados en la pagina web moderna, falta hacer el footer y esta finalizada

--- src/components/Button.jsx
+++ src/components/Button.jsx
@@ -2,7 +2,7 @@
 
 export const Button = ({styles}) => {
   return (
-    <button type="button" className={`py-4 px-6 font-poppins font-medium text-[18px] bg-blue-gradient text-primary rounded-[10px] outline-none ${styles}`}>
+    <button type="button" className={`py-4 px-6 font-poppins font-medium text-[18px] bg-blue-gradient text-primary rounded-[10px] outline-none ${styles} rounded-[10px]`}>
         Get Started
     </button>
   )
