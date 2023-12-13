5cdb717e96cf4db5f84b5262f86903f4cd046ad4
Gabriel <gabrieltorresmora2@gmail.com>
2023-12-13 07:31:54 -0300

    Finalizando proyecto de paginas web moderna dirigida hacia un banco

--- src/components/Business.jsx
+++ src/components/Business.jsx
@@ -24,7 +24,7 @@ export const Business = () => {
     <section id="features" className={layout.section}>
         <div className={`layout.sectionInfo`}>
             <h2 className={styles.heading2}>You do the bussiness, <br className="sm:block hidden"/> we'll handle the money.</h2>
-            <p className={`${styles.paragraph} max-w[470px] mt-5`}> 
+            <p className={`${styles.paragraph} max-w-[470px] mt-5`}> 
                   With the right credit card, you can improve your financial life by
                   building credit, earning rewards and saving money. But with hundreds
                   of credit cards on the market.
